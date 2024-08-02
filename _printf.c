e551a4e41607847c8848aee3844f8b124769fa7a
Anthony <peakybeetle@gmail.com>
2024-08-01 19:17:24 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -2,12 +2,12 @@
 
 /**
  * _printf - Printf function.
- * @format: format.
- * Return: Printed chars.
+ * @format: Format.
+ * Return: Printed characters.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int g, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list args;
 	char buffer[1024];
@@ -17,11 +17,11 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	for (i = 0; format && format[i]; i++)
+	for (g = 0; format && format[g]; g++)
 	{
-		if (format[i] != '%')
+		if (format[g] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = format[g];
 			if (buff_ind == 1024)
 				print_buffer(buffer, &buff_ind);
 			printed_chars++;
@@ -29,12 +29,12 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, args);
-			precision = get_precision(format, &i, args);
-			size = get_size(format, &i);
-			++i;
-			printed = delegate_print(format, &i, args, buffer,
+			flags = get_flags(format, &g);
+			width = get_width(format, &g, args);
+			precision = get_precision(format, &g, args);
+			size = get_size(format, &g);
+			++g;
+			printed = delegate_print(format, &g, args, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
